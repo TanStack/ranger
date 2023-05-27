4c49aa4c6e0305e5a35f5c9cd2261cd8a3867f83
robert <robert.kulinski93@gmail.com>
2023-05-27 18:56:30 +0200

    Support react 18

--- examples/react/basic/src/main.tsx
+++ examples/react/basic/src/main.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import { useRanger, Ranger } from '@tanstack/react-ranger'
 
 function App() {
@@ -93,9 +93,10 @@ function App() {
   )
 }
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('root')!)
+
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root'),
 )
--- examples/react/update-on-drag/src/main.tsx
+++ examples/react/update-on-drag/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 import { useRanger, Ranger } from '@tanstack/react-ranger'
+import { createRoot } from 'react-dom/client'
 
 function App() {
   const [values, setValues] = React.useState<ReadonlyArray<number>>([10])
@@ -90,9 +90,10 @@ function App() {
   )
 }
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('root')!)
+
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root'),
 )
