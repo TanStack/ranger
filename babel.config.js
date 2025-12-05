4452a59fd124e8004b8a5685eb605762fe961241
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2025-12-06 09:44:16 +1100

    ci: more cleanup

--- babel.config.js
+++ babel.config.js
@@ -18,16 +18,6 @@ module.exports = {
     '@babel/preset-typescript',
   ],
   plugins: [
-    // 'babel-plugin-transform-async-to-promises',
     cjs && ['@babel/transform-modules-commonjs', { loose }],
-    // [
-    //   '@babel/transform-runtime',
-    //   {
-    //     useESModules: !cjs,
-    //     version: require('./package.json').devDependencies[
-    //       '@babel/runtime'
-    //     ].replace(/^[^0-9]*/, ''),
-    //   },
-    // ],
   ].filter(Boolean),
 }
